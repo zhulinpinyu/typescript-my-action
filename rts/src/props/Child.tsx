03c7d7cf2aa167d8e4aaa123ffef924eef553172
zhulinpinyu <lixiangmu@gmail.com>
2021-07-19 09:25:05 +0800

    Annotations with Children

--- rts/src/props/Child.tsx
+++ rts/src/props/Child.tsx
@@ -1,7 +1,21 @@
 interface ChildProps {
   color: string;
+  onClick: () => void;
 }
 
-export const Child = (props: ChildProps) => {
-  return <div>Hi {props.color}</div>
+// Function style component define
+export const Child = ({color, onClick}: ChildProps) => {
+  return <div>
+      Hi {color}
+      <button onClick={onClick}>Click me!</button>
+    </div>
+}
+
+// React style Function Component define
+export const ChildAsFC: React.FC<ChildProps> = ({color, onClick, children}) => {
+  return <div>
+    Hi {color}
+    <button onClick={onClick}>Click me!</button>
+    {children}
+  </div>
 }
--- rts/src/props/Parent.tsx
+++ rts/src/props/Parent.tsx
@@ -1,5 +1,7 @@
-import { Child } from "./Child";
+import { ChildAsFC } from "./Child";
 
 export const Parent = () => {
-  return <Child color="red" />
+  return <ChildAsFC color="red" onClick={() => console.log("Click me")}>
+    <button>Children Btn</button>
+  </ChildAsFC>
 }
